789a19fd3b74ac48ac44a2919e6b267a398c28ec
kruglov-dmitry <kruglov.dima@gmail.com>
2023-08-31 16:59:36 +0200

    WIP adding boilerplate to generate transaction within new endpoint pay

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	filippo.io/edwards25519 v1.0.0-rc.1 // indirect
 	github.com/andres-erbsen/clock v0.0.0-20160526145045-9e14626cd129 // indirect
 	github.com/blendle/zapdriver v1.3.1 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
 	github.com/dfuse-io/logging v0.0.0-20201110202154-26697de88c79 // indirect
@@ -21,6 +22,8 @@ require (
 	github.com/gagliardetto/treeout v0.1.4 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/gorilla/rpc v1.2.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
--- routes/routes.go
+++ routes/routes.go
@@ -1,9 +1,18 @@
 package routes
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/gagliardetto/solana-go"
+	"github.com/gagliardetto/solana-go/programs/system"
+	"github.com/gagliardetto/solana-go/rpc"
+	confirm "github.com/gagliardetto/solana-go/rpc/sendAndConfirmTransaction"
+	"github.com/gagliardetto/solana-go/rpc/ws"
+	"github.com/gagliardetto/solana-go/text"
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo/v4"
 	"github.com/nikitavoloboev/sol-pay/middleware"
@@ -20,6 +29,7 @@ func RegisterRoutes(e *echo.Echo, db *gorm.DB) {
 	e.GET("/", root)
 	e.GET("/hello", hello, middleware.LoggerMiddleware, middleware.SessionMiddleware()) // Add middleware here
 	e.POST("/users", h.addUser, middleware.LoggerMiddleware)
+	e.POST("/pay", h.sendPayment, middleware.LoggerMiddleware)
 	// e.PUT("/users/:id", updateUser)
 }
 
@@ -39,22 +49,21 @@ func generateWallet() (string, string) {
 	//fmt.Println("account private key:", account.PrivateKey)
 	//fmt.Println("account public key:", account.PublicKey())
 
-	return account.PrivateKey.String(), account.PublicKey().String()
+	// Create a new RPC client:
+	client := rpc.New(rpc.DevNet_RPC)
 
-	//// Create a new RPC client:
-	//client := rpc.New(rpc.TestNet_RPC)
-	//
-	//// Airdrop 5 SOL to the new account:
-	//out, err := client.RequestAirdrop(
-	//  context.TODO(),
-	//  account.PublicKey(),
-	//  solana.LAMPORTS_PER_SOL*1,
-	//  rpc.CommitmentFinalized,
-	//)
-	//if err != nil {
-	//  panic(err)
-	//}
-	//fmt.Println("airdrop transaction signature:", out)
+	// Airdrop 5 SOL to the new account:
+	out, err := client.RequestAirdrop(
+		context.TODO(),
+		account.PublicKey(),
+		solana.LAMPORTS_PER_SOL*1,
+		rpc.CommitmentFinalized,
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("airdrop transaction signature:", out)
+	return account.PrivateKey.String(), account.PublicKey().String()
 }
 
 func (h *Handler) addUser(c echo.Context) error {
@@ -68,6 +77,106 @@ func (h *Handler) addUser(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
 	return c.JSON(http.StatusCreated, user)
+}
+
+func GetUserByID(db *gorm.DB, userID uint) (*model.User, error) {
+	var user model.User
+	if err := db.First(&user, userID).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+type UserIDInput struct {
+	SourceUserID uint `json:"source_user_id"`
+	TargetUserID uint `json:"target_user_id"`
+}
+
+func (h *Handler) sendPayment(c echo.Context) error {
+	var input UserIDInput
+	if err := c.Bind(&input); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	sourceUser, err := GetUserByID(h.DB, input.SourceUserID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	targetUser, err := GetUserByID(h.DB, input.TargetUserID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	// Create a new RPC client:
+	rpcClient := rpc.New(rpc.DevNet_RPC)
+
+	// Create a new WS client (used for confirming transactions)
+	wsClient, err := ws.Connect(context.Background(), rpc.DevNet_WS)
+	if err != nil {
+		panic(err)
+
+	}
+
+	/*FIXME - create it from db details */
+	// Load the account that you will send funds FROM:
+	accountFrom, err := solana.PrivateKeyFromSolanaKeygenFile("/path/to/.config/solana/id.json")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("accountFrom private key:", accountFrom)
+	fmt.Println("accountFrom public key:", accountFrom.PublicKey())
+
+	// The public key of the account that you will send sol TO:
+	accountTo := solana.MustPublicKeyFromBase58(targetUser.Wallet)
+
+	recent, err := rpcClient.GetRecentBlockhash(context.TODO(), rpc.CommitmentFinalized)
+	if err != nil {
+		panic(err)
+	}
+	amount := uint64(1)
+	tx, err := solana.NewTransaction(
+		[]solana.Instruction{
+			system.NewTransferInstruction(
+				amount,
+				accountFrom.PublicKey(),
+				accountTo,
+			).Build(),
+		},
+		recent.Value.Blockhash,
+		solana.TransactionPayer(accountFrom.PublicKey()),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = tx.Sign(
+		func(key solana.PublicKey) *solana.PrivateKey {
+			if accountFrom.PublicKey().Equals(key) {
+				return &accountFrom
+			}
+			return nil
+		},
+	)
+	if err != nil {
+		panic(fmt.Errorf("unable to sign transaction: %w", err))
+	}
+	spew.Dump(tx)
+	// Pretty print the transaction:
+	tx.EncodeTree(text.NewTreeEncoder(os.Stdout, "Transfer SOL"))
+
+	// Send transaction, and wait for confirmation:
+	sig, err := confirm.SendAndConfirmTransaction(
+		context.TODO(),
+		rpcClient,
+		wsClient,
+		tx,
+	)
+	if err != nil {
+		panic(err)
+	}
+	spew.Dump(sig)
+
 }
 
 /*
