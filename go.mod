b64c9b90e0df4c80c03df09df0f7d338ed89dacc
kruglov-dmitry <kruglov.dima@gmail.com>
2023-08-31 13:44:08 +0200

    Ass skeleton for dummy middleware

--- go.mod
+++ go.mod
@@ -1,3 +1,30 @@
-module github.com/nikitavoloboev/sol-pay
+module whatever.com/back
 
 go 1.21.0
+
+require (
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	github.com/go-chi/jwtauth/v5 v5.1.1 // indirect
+	github.com/go-chi/render v1.0.3 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+	github.com/labstack/echo/v4 v4.11.1 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.1 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/httprc v1.0.4 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/jwx/v2 v2.0.11 // indirect
+	github.com/lestrrat-go/option v1.0.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/segmentio/asm v1.2.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,90 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/labstack/echo/v4"
+	"github.com/lestrrat-go/jwx/v2/jwk"
 )
 
-func main() {
-	fmt.Println("test")
+func SessionMiddleware() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			cookie, err := c.Cookie("hanko")
+			if err == http.ErrNoCookie {
+				return c.Redirect(http.StatusTemporaryRedirect, "/unauthorized")
+			}
+			if err != nil {
+				return err
+			}
+			hankoApiURL := "https://e879ccc9-285e-49d3-b37e-b569f0db4035.hanko.io"
+			// replace "hankoApiURL" with your API URL
+			set, err := jwk.Fetch(
+				context.Background(),
+				fmt.Sprintf("%v/.well-known/jwks.json", hankoApiURL),
+			)
+			if err != nil {
+				return err
+			}
+
+			token, err := jwt.Parse([]byte(cookie.Value), jwt.WithKeySet(set))
+			if err != nil {
+				return c.Redirect(http.StatusTemporaryRedirect, "/unauthorized")
+			}
+
+			log.Printf("session for user '%s' verified successfully", token.Subject())
+
+			c.Set("token", cookie.Value)
+			c.Set("user", token.Subject())
+
+			return next(c)
+		}
+	}
 }
+
+// var tokenAuth *jwtauth.JWTAuth
+//
+//	func init() {
+//		// You can use any string value as a "secret key". In production, use a more secure method to store and retrieve it.
+//		tokenAuth = jwtauth.New("HS256", []byte("secret_key"), nil)
+//	}
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(SessionMiddleware)
+	r.Get("/hello", helloHandler)
+
+	// Public routes
+	//r.Get("/auth", authHandler)
+
+	//// Protected routes
+	//r.Group(func(r chi.Router) {
+	//	r.Use(jwtauth.Verifier(tokenAuth))
+	//	r.Use(jwtauth.Authenticator)
+
+	//	r.Get("/protected", protectedHandler)
+	//})
+
+	http.ListenAndServe(":8080", r)
+}
+
+//func authHandler(w http.ResponseWriter, r *http.Request) {
+//	_, tokenString, _ := tokenAuth.Encode(jwt.MapClaims{"user_id": 123})
+//
+//	render.JSON(w, r, map[string]string{"token": tokenString})
+//}
+//
+//func protectedHandler(w http.ResponseWriter, r *http.Request) {
+//	_, claims, _ := jwtauth.FromContext(r.Context())
+//	render.JSON(w, r, claims)
+//}
+//
